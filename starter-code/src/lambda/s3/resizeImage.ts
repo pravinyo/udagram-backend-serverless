15c1b3b0c0ca16e28cd258ddabda865497b27313
Pravinyo <pravinyo12@gmail.com>
2020-04-03 20:17:03 +0530

    first commit

---
+++ starter-code/src/lambda/s3/resizeImage.ts
@@ -0,0 +1,14 @@
+import { SNSEvent, SNSHandler } from 'aws-lambda'
+import 'source-map-support/register'
+//import * as AWS from 'aws-sdk'
+//import Jimp from 'jimp/es'
+
+//const s3 = new AWS.S3()
+
+//const imagesBucketName = process.env.IMAGES_S3_BUCKET
+//const thumbnailBucketName = process.env.THUMBNAILS_S3_BUCKETnpm
+
+export const handler: SNSHandler = async (event: SNSEvent) => {
+    console.log("Event received in resizeImage:",JSON.stringify(event));
+    
+}
