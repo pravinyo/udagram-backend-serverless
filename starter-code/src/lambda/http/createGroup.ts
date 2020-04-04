7e98fffcc284835faa164bc755b6437085acb9f4
Pravinyo <pravinyo12@gmail.com>
2020-04-04 15:21:24 +0530

    token verification added before creation

--- starter-code/src/lambda/http/createGroup.ts
+++ starter-code/src/lambda/http/createGroup.ts
@@ -5,6 +5,7 @@ import * as uuid from 'uuid'
 
 import * as middy from 'middy'
 import { cors } from 'middy/middlewares'
+import { getUserId } from '../../auth/utils'
 
 const docClient = new AWS.DynamoDB.DocumentClient()
 const groupsTable = process.env.GROUPS_TABLE
@@ -12,11 +13,16 @@ const groupsTable = process.env.GROUPS_TABLE
 export const handler = middy(async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   console.log('Processing event: ', event)
   const itemId = uuid.v4()
-
+  const authorization = event.headers.Authorization
+  const split = authorization.split(' ')
+  const jwtToken = split[1]
+  
   const parsedBody = JSON.parse(event.body)
+  const userId = getUserId(jwtToken)
 
   const newItem = {
     id: itemId,
+    userId:userId,
     ...parsedBody
   }
 
