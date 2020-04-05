2f278ece5ebbfd60270cb6b328918229791d8b99
Pravinyo <pravinyo12@gmail.com>
2020-04-05 12:18:42 +0530

    uses domain ,data layer to create and access group

--- starter-code/src/lambda/http/createGroup.ts
+++ starter-code/src/lambda/http/createGroup.ts
@@ -1,35 +1,22 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 import 'source-map-support/register'
-import * as AWS  from 'aws-sdk'
-import * as uuid from 'uuid'
 
 import * as middy from 'middy'
 import { cors } from 'middy/middlewares'
-import { getUserId } from '../../auth/utils'
 
-const docClient = new AWS.DynamoDB.DocumentClient()
-const groupsTable = process.env.GROUPS_TABLE
+
+import { CreateGroupRequest } from '../../requests/CreateGroupRequest'
+import { createGroup } from '../../businessLogic/groups'
 
 export const handler = middy(async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   console.log('Processing event: ', event)
-  const itemId = uuid.v4()
+  
+  const newGroup: CreateGroupRequest = JSON.parse(event.body)
   const authorization = event.headers.Authorization
   const split = authorization.split(' ')
   const jwtToken = split[1]
-  
-  const parsedBody = JSON.parse(event.body)
-  const userId = getUserId(jwtToken)
 
-  const newItem = {
-    id: itemId,
-    userId:userId,
-    ...parsedBody
-  }
-
-  await docClient.put({
-    TableName: groupsTable,
-    Item: newItem
-  }).promise()
+  const newItem = await createGroup(newGroup, jwtToken)
 
   return {
     statusCode: 201,
